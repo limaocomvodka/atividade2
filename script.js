2061f209f6d0b8dbd860179ce07a1bc391a763f3
limaocomvodka <104787948+limaocomvodka@users.noreply.github.com>
2022-06-17 19:51:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let idade = 31
+if(idade>18){
+    console.log ("Você pode ser um Vingador")
+}else{
+   console.log ("Você não pode entrar nos Vingadores")
+}
+
+let age=31
+let human = true
+if((age>18)&&(human===true)){
+    console.log("Enter the Matrix, Neo")
+}else{
+    console.log("a revolta das máquinas é inevitável")
+}
+
+let mes = "Abril"
+if(mes==="Janeiro" || mes==="Dezembro"){
+    console.log("Você não vai passar!!!")
+}else{
+    console.log("fly, you fools!")
+}
+
+let nome= "Limão"
+if(nome[0]==="R"){
+    console.log("Seu nome começa com R")
+}else{
+    console.log("Seu nome não começa com R")
+}
+
+let sobrenome = "Menchini"
+if((sobrenome.length>6)||(nome[0]==="E")){
+    console.log("Você pode ter acesso.")
+}else{
+    console.log("Você será barrado")
+}
